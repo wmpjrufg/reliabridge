bc93966819fcf7224f1338d26ce8f8e2a3304a3f
wmpjrufg <wanderlei_junior@ufcat.edu.br>
2026-01-27 10:52:49 -0600

    feat: logo main page

--- pages/home.py
+++ pages/home.py
@@ -9,10 +9,15 @@ textos = {
                         "lang_label": "Idioma/Language",
                         "lang_opts": ["Português", "English"],
                         "title": "Bem-vindo",
-                        "body": (
+                        "logo_ufcat": "imgs/UFCAT_Logo.jpg",
+                        "logo_ufscar": "imgs/UFSCAR_Logo.jpg",
+                        "body": (   
                                     "Esta plataforma realiza análises estruturais em pontes de madeira, seguindo rigorosamente as "
                                     "especificações da **NBR 7190:2022 - Projeto de Estruturas de Madeira**. "
-                                    "Ele permite avaliar a segurança e o desempenho de longarinas e vigas de madeira, calculando propriedades "
+                                    "Esta plataforma foi realizada pela equipe composta por: Wanderlei M. Pereira Junior, "
+                                    "Priscilla Silva Teotônio, Pedro Henrique Gomes Duarte, Wellington A. da Silva, André Luís Christoforo, "
+                                    "Matheus Henrique Morato de Moraes, João Paulo M. Lopes, Enzo e Maria José. "
+                                    "Ela permite avaliar a segurança e o desempenho de longarinas e vigas de madeira, calculando propriedades "
                                     "geométricas de seções retangulares e circulares, determinando esforços solicitantes (momentos fletores, "
                                     "cortantes e reações) considerando cargas permanentes e variáveis, e aplicando o coeficiente de impacto "
                                     "vertical conforme a NBR 7188:2024. O software realiza verificações de estado limite último, incluindo "
@@ -26,35 +31,50 @@ textos = {
                         "lang_label": "Language/Idioma",
                         "lang_opts": ["Português", "English"],
                         "title": "Welcome",
+                        "logo_ufcat": "imgs/UFCAT_Logo.jpg",
+                        "logo_ufscar": "imgs/UFSCAR_Logo.jpg",
                         "body": (
                                     "This platform performs structural analyses of timber bridges, rigorously following the specifications of "
-                                    "**NBR 7190:2022 - Design of Timber Structures**. It enables the assessment of safety and performance of "
-                                    "timber stringers and beams by calculating geometric properties of rectangular and circular cross-sections, "
-                                    "determining internal forces (bending moments, shear forces, and reactions) considering permanent and "
-                                    "variable loads, and applying the vertical impact coefficient in accordance with NBR 7188:2024. The software "
-                                    "performs ultimate limit state verifications, including oblique bending and shear, and serviceability limit "
-                                    "state checks, such as maximum deflections. Furthermore, it incorporates a reliability analysis using the "
-                                    "FORM method to estimate the reliability index and probability of failure, accounting for inherent "
-                                    "uncertainties in materials and loads, making it an essential tool for the design and verification of timber "
-                                    "bridges within the context of Brazilian technical standards."
+                                    "**NBR 7190:2022 - Design of Timber Structures**. This platform was developed by the team: "
+                                    "Wanderlei M. Pereira Junior, Priscilla Silva Teotônio, Pedro Henrique Gomes Duarte, Wellington A. da Silva, "
+                                    "André Luís Christoforo, Matheus Henrique Morato de Moraes, João Paulo M. Lopes, Enzo, and Maria José. "
+                                    "It enables the assessment of safety and performance of stringers and beams by calculating geometric properties"
+                                    "of rectangular and circular cross-sections, determining internal forces (bending moments, shear forces, "
+                                    "and reactions) considering permanent and variable loads, and applying the vertical impact coefficient in "
+                                    "accordance with NBR 7188:2024. The software performs ultimate limit state verifications, including oblique "
+                                    "bending and shear, and serviceability limit state checks, such as maximum deflections. Furthermore, "
+                                    "it incorporates a reliability analysis using the FORM method to estimate the reliability index and probability "
+                                    "of failure, accounting for inherent uncertainties in materials and loads, making it an essential tool for "
+                                    "the design and verification of timber bridges within the context of Brazilian technical standards."
                                 ),
                     },
         }
 
 lang = st.session_state["lang"]
 t = textos.get(lang, textos["pt"])
+idx = 0 if lang == "pt" else 1
 
-idx = 0 if st.session_state["lang"] == "pt" else 1
-
-col1, col2 = st.columns([1, 3])
-with col1:
+with st.sidebar:
     escolha = st.selectbox(t["lang_label"], t["lang_opts"], index=idx)
+    novo_lang = "pt" if escolha == "Português" else "en"
+    if novo_lang != st.session_state["lang"]:
+        st.session_state["lang"] = novo_lang
+        st.rerun()
 
-novo_lang = "pt" if escolha == "Português" else "en"
-if novo_lang != st.session_state["lang"]:
-    st.session_state["lang"] = novo_lang
-    st.rerun()
+    
+
 
 # --- Conteúdo ---
+try:
+    col_logo1, col_logo2 = st.columns([0.07, 1])
+
+    with col_logo1:
+        st.image(t["logo_ufcat"], width=200)
+
+    with col_logo2:
+        st.image(t["logo_ufscar"], width=100)
+except:
+    st.warning("Arquivo de imagem não encontrado. Verifique a pasta 'images/'.")
+    
 st.title(t["title"])
 st.write(t["body"])
