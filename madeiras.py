8ca002cd5480a7d7f7c2b034c8bb026ae28fdfae
wmpjrufg <wanderlei_junior@ufcat.edu.br>
2026-01-31 14:25:13 -0600

    chore: import bytesIO

--- madeiras.py
+++ madeiras.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 import io
+from io import BytesIO
 from scipy import stats as st
 
 from UQpy.distributions import TruncatedNormal
